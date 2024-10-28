831e68762e0477617922e8e2f86f342454372ded
64r3th <64r3th@gmail.com>
2024-10-28 17:29:08 -0600

    Mon, Oct 28, 2024  5:29:08 PM

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,10 @@
 const lists = {};
 const currentList = {
-  name: "Example List",
-  tasks: [{text: "Example Item", completed: false}]
-}
-selectList = (key) => {
+  name: "Example",
+  tasks: [],
+  id: generateID()
+};
+selectList = key => {
   currentList["name"] = lists[key]["name"];
   currentList["tasks"] = lists[key]["tasks"];
   currentList["id"] = lists[key]["id"];
@@ -13,37 +14,34 @@ selectList = (key) => {
 addList = () => {
   lists[Object.keys(lists).length] = {
     name: document.getElementById("newListName").value,
-    tasks: []
+    tasks: [],
+    ID: generateID()
   }
   render();
 }
-removeList = (key) => {
-  delete lists.key;
+removeList = key => {
+  delete lists[key];
   render();
 }
-addTask = (name) => {
-  currentList[tasks].push({
+addTask = name => {
+  currentList["tasks"].push({
     text: name,
-    completed: false
+    completed: false,
+    ID: generateID()
   });
   render();
 }
-removeTask = (index) => {
-  currentList[tasks].splice(index, 1);
+removeTask = index => {
+  currentList.tasks.splice(index, 1);
   render();
 }
-toggleTaskCompleted = (index) => {
-  currentList[tasks][index][completed] = !currentList[tasks][index][completed];
+toggleTaskCompleted = index => {
+  currentList.tasks[index].completed = !currentList.tasks[index].completed;
   render();
 }
 removeAllCompletedTasks = () => {
-  const buffer = [];
-  for (let i = 0; i < currentList[tasks].length; i++) {
-    if (currentList[tasks][i].completed) {
-      buffer.push(i);
-    }
-  }
-  buffer.forEach((j) => removeTask(j))
+  currentList.tasks = currentList.tasks.filter(task => !task.completed);
+  render();
 }
 render = () => {
   let listsHTML = '<ul class="listGroup">';
@@ -53,8 +51,25 @@ render = () => {
 
   document.getElementById('lists').innerHTML = listsHTML + '</ul>';
   document.getElementById('currentListName').innerText = currentList.name;
-  let tasksHTML = '<ul class="listGroupFlush">'
-  currentList.tasks.forEach((task) => tasksHTML += `<li class="listGroupItem">${task.text}</li>`)
-
-  document.getElementById('currentListTasks').innerHTML = tasksHTML + '</ul>';
+  
+  if (Object.keys(currentList).length > 0) {
+    let tasksHTML = '<ul class="listGroupFlush">'
+    for (let i = 0; i < currentList.tasks.length; i++ ) {
+      tasksHTML += `<li class="listGroupItem completed-${currentList.tasks[i].completed}" id="${generateID()}" onclick="toggleTaskCompleted(${i})">${currentList.tasks[i].text}</li>`
+    }
+    document.getElementById('currentListTasks').innerHTML = tasksHTML + '</ul>';
+  }
+  save();
 }
+
+save = () => {
+  localStorage.setItem('currentList', JSON.stringify(currentList));
+  localStorage.setItem('lists', JSON.stringify(lists));
+}
+
+function generateID() {
+  let ID = "";
+  for(let i = 0; i < 16; i++) {ID += "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"[Math.floor(Math.random() * 36)]}
+  return ID;
+}//7,958,661,109,946,400,884,391,936
+console.log(generateID())
